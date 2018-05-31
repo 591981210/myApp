2dfaa8293758295743c8e4ae50edf573bf462586
cherish <591981210@qq.com>
2018-05-31 15:34:55 +0800

    体验 tab-navigator

--- index.ios.js
+++ index.ios.js
@@ -4,50 +4,86 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    StyleSheet,
+    Text,
+    Navigator,
+    Image,
+    View
 } from 'react-native';
+import TabNavigator from 'react-native-tab-navigator';
 
 export default class myApp extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+    constructor(props) {
+        super(props);
+        //初始向
+        this.state = {
+            selectedTab: 'tb_popular',
+        }
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                {<TabNavigator>
+                    <TabNavigator.Item
+                        selected={this.state.selectedTab === 'tb_popular'}
+                        selectedTitleStyle={{color: 'red'}}
+                        title="最热"
+                        renderIcon={() => <Image style={styles.image} source={require('./res/images/ic_polular.png')}/>}
+                        renderSelectedIcon={() => <Image style={[styles.image, {tintColor: 'red'}]}
+                                                         source={require('./res/images/ic_polular.png')}/>}
+                        onPress={() => this.setState({selectedTab: 'tb_popular'})}>
+                        <View style={{backgroundColor: 'red', flex: 1}}></View>
+                    </TabNavigator.Item>
+                    <TabNavigator.Item
+                        selected={this.state.selectedTab === 'tb_trending'}
+                        title="趋势"
+                        selectedTitleStyle={{color: 'yellow'}}
+                        renderIcon={() => <Image style={styles.image}
+                                                 source={require('./res/images/ic_trending.png')}/>}
+                        renderSelectedIcon={() => <Image style={[styles.image, {tintColor: 'yellow'}]}
+                                                         source={require('./res/images/ic_trending.png')}/>}
+                        onPress={() => this.setState({selectedTab: 'tb_trending'})}>
+                        <View style={{backgroundColor: 'yellow', flex: 1}}></View>
+                    </TabNavigator.Item>
+                    <TabNavigator.Item
+                        selected={this.state.selectedTab === 'tb_favorite'}
+                        title="收藏"
+                        selectedTitleStyle={{color: 'green'}}
+                        renderIcon={() => <Image style={styles.image}
+                                                 source={require('./res/images/ic_favorite.png')}/>}
+                        renderSelectedIcon={() => <Image style={[styles.image, {tintColor: 'green'}]}
+                                                         source={require('./res/images/ic_favorite.png')}/>}
+                        onPress={() => this.setState({selectedTab: 'tb_favorite'})}>
+                        <View style={{backgroundColor: 'green', flex: 1}}></View>
+                    </TabNavigator.Item>
+                    <TabNavigator.Item
+                        selected={this.state.selectedTab === 'tb_my'}
+                        title="我的"
+                        selectedTitleStyle={{color: 'blue'}}
+                        renderIcon={() => <Image style={styles.image} source={require('./res/images/ic_my.png')}/>}
+                        renderSelectedIcon={() => <Image style={[styles.image, {tintColor: 'blue'}]}
+                                                         source={require('./res/images/ic_my.png')}/>}
+                        onPress={() => this.setState({selectedTab: 'tb_my'})}>
+                        <View style={{backgroundColor: 'blue', flex: 1}}></View>
+                    </TabNavigator.Item>
+                </TabNavigator>}
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+    },
+    image: {
+        height: 22,
+        width: 22,
+    }
 });
 
 AppRegistry.registerComponent('myApp', () => myApp);
