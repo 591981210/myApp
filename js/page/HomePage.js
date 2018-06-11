e61c1c8a46ccad792c9bd51fa6970d576121181c
cherish <591981210@qq.com>
2018-06-11 17:27:25 +0800

    使用 AsyncStorage 来替代 localStorage

--- js/page/HomePage.js
+++ js/page/HomePage.js
@@ -13,6 +13,7 @@ import {
 
 import TabNavigator from 'react-native-tab-navigator';
 import PopularPage from './PopularPage'
+import AsyncStorageTest from '../../AsyncStorageTest'
 export default class HomePage extends Component {
     constructor(props) {
         super(props);
@@ -45,7 +46,7 @@ export default class HomePage extends Component {
                         renderSelectedIcon={() => <Image style={[styles.image, {tintColor: 'yellow'}]}
                                                          source={require('../../res/images/ic_trending.png')}/>}
                         onPress={() => this.setState({selectedTab: 'tb_trending'})}>
-                        <View style={{backgroundColor: 'yellow', flex: 1}}></View>
+                        <AsyncStorageTest></AsyncStorageTest>
                     </TabNavigator.Item>
                     <TabNavigator.Item
                         selected={this.state.selectedTab === 'tb_favorite'}
