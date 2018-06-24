bc3f1980ee17f0334a45cd4bc68f23bd96f8f911
cherish <591981210@qq.com>
2018-06-24 18:54:11 +0800

    扩展语言排序和语言筛选功能

--- js/page/my/MyPage.js
+++ js/page/my/MyPage.js
@@ -11,6 +11,7 @@ import {
 import CustomKeyPage from './CustomKeyPage'
 import SortKeyPagePage from './SortKeyPagePage'
 import NavigationBar from '../../../js/common/NavigationBar'
+import {FLAG_LANGUAGE} from '../../expand/dao/LanguageDao'
 export default class MyPage extends Component {
     constructor(props) {
         super(props);
@@ -30,6 +31,7 @@ export default class MyPage extends Component {
                             component:CustomKeyPage,
                             params:{
                                 ...this.props,
+                                flag:FLAG_LANGUAGE.flag_key
                             }
                         })
                     }}
@@ -40,14 +42,43 @@ export default class MyPage extends Component {
                     style={styles.tips}
                     onPress={()=>{
                         this.props.navigator.push({
+                            component:CustomKeyPage,
+                            params:{
+                                ...this.props,
+                                flag:FLAG_LANGUAGE.flag_language
+                            }
+                        })
+                    }}
+                >
+                    自定义语言
+                </Text>
+                <Text
+                    style={styles.tips}
+                    onPress={()=>{
+                        this.props.navigator.push({
                             component:SortKeyPagePage,
                             params:{
-                                ...this.props
+                                ...this.props,
+                                flag:FLAG_LANGUAGE.flag_key
                             }
                         })
                     }}
                 >
                     标签排序
+                </Text>
+                <Text
+                    style={styles.tips}
+                    onPress={()=>{
+                        this.props.navigator.push({
+                            component:SortKeyPagePage,
+                            params:{
+                                ...this.props,
+                                flag:FLAG_LANGUAGE.flag_language
+                            }
+                        })
+                    }}
+                >
+                    语言排序
                 </Text>
                 <Text
                     style={styles.tips}
